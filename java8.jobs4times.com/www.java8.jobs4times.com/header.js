30803fff01431584ac364d277e6c3ba682f04310
sanju <s4njua@gmail.com>
2024-06-23 07:57:19 +0530

    first commit

---
+++ java8.jobs4times.com/www.java8.jobs4times.com/header.js
@@ -0,0 +1,12 @@
+document.write('\
+\
+			<table  bgcolor="" border="0" cellpadding="0" cellspacing="0" width="100%">\
+				<tbody>\
+					<tr>\
+						<td align="left" width="50%"><a href="http://www.jobs4times.com/"><img alt="jobs4timesLogo" height="100" src="https://img.logos.jobs4times.com/banner J4T.jpg" width="300" /> </a></td>\
+						<td align="right"><a href="https://jobs4times.com/J4T/allmaterial.html"><img alt="jobs4timesLogo" height="100" src="https://img.logos.jobs4times.com/as2.jpg" width="100" /> </a></td>\
+					</tr>\
+				</tbody>\
+			</table>\
+\
+');
